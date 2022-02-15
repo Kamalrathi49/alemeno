31dc4654aba648370743bd315dedad59b476e9eb
Kamal Rathi <kamalrathi049@gmail.com>
2022-02-15 21:34:20 +0530

    updated

--- myproject/settings.py
+++ myproject/settings.py
@@ -140,8 +140,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'thejamesbond49@gmail.com'
-EMAIL_HOST_PASSWORD = 'bbrwlkfttnhcjsdd' 
+EMAIL_HOST_USER = 'your gmail'
+EMAIL_HOST_PASSWORD = 'your password' 
 
 
 MESSAGE_TAGS = {
